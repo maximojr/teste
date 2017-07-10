a1b2bacfe5fcf42faaeb4c18e1e657f716614b05
Francisco <francisco.maximo@discover.com.br>
2017-07-10 09:57:43 -0300

    comit inicial

---
+++ src/main/java/br/com/discover/teste/jenkins/HomeServlet.java
@@ -0,0 +1,35 @@
+package br.com.discover.teste.jenkins;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/")
+public class HomeServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6946516122386443712L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		req.setAttribute("hora", new SimpleDateFormat("HH:MM:ss dd/mm/yyyy").format(Calendar.getInstance().getTime()));
+		
+		req.getRequestDispatcher("WEB-INF/teste.jsp").forward(req, resp);
+	}
+
+}
