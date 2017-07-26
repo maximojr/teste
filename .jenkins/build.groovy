1ac4d0266a7fd9e0301563badb3b56454b7f0033
Francisco <francisco.maximo@discover.com.br>
2017-07-26 14:22:09 -0300

    .

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -4,7 +4,7 @@ node {
       checkout scm
 	  sh "whoami"
 	  sh "ifconfig"
-	  sh "find / -name 'docker'"
+	  sh "sudo find / -name 'docker'"
 	  // save our docker build context before we switch branches
 	  sh "cp -r ./.docker/build tmp-docker-build-context"
 	  
