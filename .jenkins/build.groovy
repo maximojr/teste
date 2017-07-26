89633e1a2306f5850e6ab09d540ba1cf728b6a11
Francisco <francisco.maximo@discover.com.br>
2017-07-26 16:33:25 -0300

    .

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -13,7 +13,8 @@ node('docker_slave') {
    stage('Build') {
       
       if (isUnix()) {
-         sh "'${mvnHome}/bin/mvn' -DskipTests clean package"
+         //sh "'${mvnHome}/bin/mvn' -DskipTests clean package"
+		 sh "mvn -DskipTests clean package"
       } else {
          bat(/"${mvnHome}\bin\mvn" -DskipTests clean package/)
       }
@@ -21,7 +22,8 @@ node('docker_slave') {
    stage('Testes unitários') {
        
        if (isUnix()) {
-         sh "'${mvnHome}/bin/mvn' test"
+         //sh "'${mvnHome}/bin/mvn' test"
+		 sh "mvn test"
       } else {
          bat(/"${mvnHome}\bin\mvn" test/)
       }
@@ -55,7 +57,9 @@ node('docker_slave') {
    stage('Deploy'){
        if (env.CONFIRMA_DEPLOY=='sim'){
 		   
-		  sh "cp /var/lib/docker/volumes/proxy-reverso_jenkins-vol/_data/workspace/pipeline_1/target/teste.war ./tmp-docker-build-context"
+		  //sh "cp /var/lib/docker/volumes/proxy-reverso_jenkins-vol/_data/workspace/pipeline_1/target/teste.war ./tmp-docker-build-context"
+		  
+		  sh "cp target/teste.war ./tmp-docker-build-context"
           withDockerContainer([image: "tehranian/dind-jenkins-slave"]){
 			  withDockerServer([uri: ""]) {
 				 withDockerRegistry([url: ""]) {
